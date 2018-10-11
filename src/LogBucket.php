36fa2e623545142e7ec87a9637126a2e0150db22
Sinevia <info@sinevia.com>
2018-10-11 07:05:32 +0100

    Update LogBucket.php

--- src/LogBucket.php
+++ src/LogBucket.php
@@ -1,14 +1,16 @@
 <?php
-class LogBucket{
+
+class LogBucket {
+
     private static $url = 'http://logbucket.org/log.php';
 
     /**
      * Logs messages with an arbitrary level.
      * @return string string response from LogBucket or false on failiure
      */
-    public static function log($key,$message,$level='debug'){
-        $url = self::$url.'?key='.rawurlencode($key).'&message='.  rawurlencode(base64_encode($message)).'&level='.rawurlencode($level);
-        $result = @file_get_contents($url);
+    public static function log($key, $message, $level = 'debug') {
+        $url = self::$url . '?key=' . rawurlencode($key) . '&message=' . rawurlencode(base64_encode($message)) . '&level=' . rawurlencode($level);
+        $result = file_get_contents($url);
         return $result;
     }
 
@@ -18,7 +20,7 @@ class LogBucket{
      * database unavailable, etc. This should rigger the SMS alerts and
      * wake you up.
      */
-    public static function alert($key,$message){
+    public static function alert($key, $message) {
         return self::log($key, $message, 'alert');
     }
 
@@ -26,7 +28,7 @@ class LogBucket{
      * Logs debug messages
      * Standard debug messages.
      */
-    public static function debug($key,$message){
+    public static function debug($key, $message) {
         return self::log($key, $message, 'debug');
     }
 
@@ -35,7 +37,7 @@ class LogBucket{
      * Critical conditions. Example: Application component unavailable,
      * unexpected exception.
      */
-    public static function critical($key,$message){
+    public static function critical($key, $message) {
         return self::log($key, $message, 'critical');
     }
 
@@ -43,7 +45,7 @@ class LogBucket{
      * Logs emergency messages
      * System is unusable.
      */
-    public static function emergency($key,$message){
+    public static function emergency($key, $message) {
         return self::log($key, $message, 'emergency');
     }
 
@@ -52,7 +54,7 @@ class LogBucket{
      * Runtime errors that do not require immediate action but should typically
      * be logged and monitored.
      */
-    public static function error($key,$message){
+    public static function error($key, $message) {
         return self::log($key, $message, 'error');
     }
 
@@ -60,7 +62,7 @@ class LogBucket{
      * Logs informational messages
      * Interesting events. Example: User logs in, SQL logs.
      */
-    public static function info($key,$message){
+    public static function info($key, $message) {
         return self::log($key, $message, 'info');
     }
 
@@ -68,7 +70,7 @@ class LogBucket{
      * Logs notice messages
      * Normal but significant events.
      */
-    public static function notice($key,$message){
+    public static function notice($key, $message) {
         return self::log($key, $message, 'notice');
     }
 
@@ -76,7 +78,8 @@ class LogBucket{
      * Logs warning messages
      * Exceptional occurrences that are not errors.
      */
-    public static function warning($key,$message){
+    public static function warning($key, $message) {
         return self::log($key, $message, 'warning');
     }
+
 }
